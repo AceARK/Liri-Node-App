917421b191be30cc212b8ac4ea44d718518503b6
Ace ARK <ace@Akhilas-MacBook-Pro.local>
2017-02-09 16:08:50 -0500

    Adding keys in export file and accessing them in liri.js.

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log("This is lOaDeD!");
+
+exports.twitterKeys = {
+	consumer_key: 'yO0EeJo7K540Am3MKDMJFbAGk',
+	consumer_secret: 'rno3rVswscQF6b21hXc2pmjmTPpnBaPP4h6Kkn3TrPYn9Xh6tT',
+	access_token_key: '3018092843-mfS5jOe3sy7EmGCxShwvL6UzlfHslBIcVIzqAXq',
+	access_token_secret: 'AAK33lorHYhFclE6hUgYsC9FoVhXaNzSFP4SUbM8FU3ii',
+}
---
+++ liri.js
@@ -0,0 +1,8 @@
+var twitterRequire = require("./keys.js");
+
+var twitter_consumer_key = twitterRequire.twitterKeys.consumer_key;
+var twitter_consumer_secret = twitterRequire.twitterKeys.consumer_secret
+var twitter_access_token_key = twitterRequire.twitterKeys.access_token_key;
+var twitter_access_token_secret = twitterRequire.twitterKeys.access_token_secret;
+
+console.log(twitterRequire.twitterKeys.consumer_key);
