86022ee5161c822d0e2629ce317bfe30c62d072d
Ace ARK <ace@Akhilas-MacBook-Pro.local>
2017-02-09 19:25:58 -0500

    Getting a list of artists for the same song.

--- liri.js
+++ liri.js
@@ -9,6 +9,26 @@ var twitterDetails = {
 }
 
 var command = process.argv[2];
+var parameter = "";
+var spotify = require('spotify');
 
-console.log(command);
+for(var i = 3; i < process.argv.length; i++) {
+	parameter += process.argv[i] + " ";
+}
 
+switch(command) {
+	case "spotify-this-song":
+		spotify.search({ type: 'track', query: parameter}, function(err, data) {
+		    if ( err ) {
+		        console.log('Error occurred: ' + err);
+		        return;
+		    }
+		 	var songDetailArray = data.tracks.items;
+		 	// console.log(songDetailArray);
+		 	songDetailArray.forEach(function(item) {
+		 		console.log(item.artists[0].name);
+		 	});
+		 	// console.log(songDetailArray);
+		    // console.log("Artist name: " + JSON.stringify(data.tracks.items[0].artists[0].name, null, 2));
+		});
+}
