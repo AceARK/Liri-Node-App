091e87102db13abadfd6811e9c586962e0a9327d
Ace ARK <ace@Akhilas-MacBook-Pro.local>
2017-02-12 19:58:22 -0500

    Refactored code to use string iterpolation instead of plain ol' multiple console logs with quotes.

--- liri.js
+++ liri.js
@@ -86,7 +86,13 @@ Album: ${newArray[0].album.name}
 
 ------------------------------`);
 		 	// Logging data
-		 	fs.appendFile("log.txt", "\n" + newArray[0].name +"\n"+newArray[0].artists[0].name+"\n"+ newArray[0].preview_url+"\n"+ newArray[0].album.name, function(error) {
+		 	fs.appendFile("log.txt", `
+${newArray[0].name}
+${newArray[0].artists[0].name}
+${newArray[0].preview_url}
+${newArray[0].album.name}
+
+`, function(error) {
 				if(error) {
 					console.log(error);
 				}
@@ -101,24 +107,29 @@ function fetchMyTweets() {
 	// API call to fetch tweets of given username
 	client.get('statuses/user_timeline', params, function(error, tweets, response) {
 		if (!error) {
-			console.log("Here are your latest 20 tweets - ");
-			console.log("===============================");
+			console.log(`
+Here are your latest 20 tweets - 
+===============================`);
 			// Get and print tweets
 			tweets.every(function(item, index) {
 				// Cutting off latest tweets at 20
 			    if (index < 20) {
 			    	// Logging tweet and info
-					fs.appendFile("log.txt", "\n" + "-----------------------" + "\n" + moment(item.created_at).format('llll') + "\n" + item.text + "\n" + "-----------------------" , function(error) {
+					fs.appendFile("log.txt", `
+-----------------------
+${moment(item.created_at).format('llll')}
+${item.text} 
+-----------------------
+` , function(error) {
 						if(error) {
 							console.log(error);
 						}
-					})
-				    console.log(" ");
-					console.log(moment(item.created_at).format('llll'));
-					console.log(" ");
-					console.log(item.text);
-					console.log(" ");
-					console.log("===============================");
+					});
+					console.log(
+`${moment(item.created_at).format('llll')}
+${item.text} 
+===============================
+`					);
 			    	return true;
 			    }
 				
@@ -129,39 +140,54 @@ function fetchMyTweets() {
 
 function fetchMovieDetails(movieName) {
 	if(movieName === "") {
-		console.log("----------------------");
-		console.log('If you haven\'t watched "Mr. Nobody," then you should: http://www.imdb.com/title/tt0485947/');
-		console.log("It's on Netflix!");
-		console.log("----------------------");
+		console.log(`
+----------------------
+If you haven't watched "Mr. Nobody," then you should: http://www.imdb.com/title/tt0485947/
+It's on Netflix!
+----------------------
+		`);
 	}else {
 		movieName = movieName.trim().split(" ").join("+");
 		request('http://www.omdbapi.com/?t=' + movieName + '&tomatoes=true', function (error, response, body) {
 			// If no error and statusCodes are either success 
 			if (!error && (response.statusCode == 200)) {
 				var data = JSON.parse(body);
-				console.log("========================");
-			    console.log("Title: " + data.Title);
-			    console.log("Released in: " + data.Year);
-			    console.log("Rated: " + data.Rated);
-			    console.log("Country: " + data.Country);
-			    console.log("Language: " + data.Language);
-			    console.log("Plot: " + data.Plot);
-			    console.log("Actors: " + data.Actors);
-			    console.log("Rotten Tomatoes Rating: " + data.tomatoRating);
-			    console.log("Rotten Tomatoes URL: " + data.tomatoURL);
+				console.log(`
+========================
+Title: ${data.Title}
+Released in: ${data.Year}
+Rated: ${data.Rated}
+Country: ${data.Country}
+Language: ${data.Language}
+Plot: ${data.Plot}
+Actors: ${data.Actors}
+Rotten Tomatoes Rating: ${data.tomatoRating}
+Rotten Tomatoes URL: ${data.tomatoURL}
+========================
+				`);
 			    // Log data
-			    fs.appendFile("log.txt", "\n" + "-----------------------" + "\n" + data.Title + "\n" + data.Year + "\n"+ data.Rated + "\n" + data.Country + "\n" + data.Language + "\n" + data.Actors + "\n"+ data.Plot + "\n" + data.tomatoRating + "\n" + data.tomatoURL + "\n" + "-----------------------" , function(error) {
+			    fs.appendFile("log.txt", `
+-----------------------
+${data.Title}
+${data.Year}
+${data.Rated}
+${data.Country}
+${data.Language}
+${data.Actors} 
+${data.Plot} 
+${data.tomatoRating}
+${data.tomatoURL}
+-----------------------` , function(error) {
 					if(error) {
 						console.log(error);
 					}
 				})
-			    console.log("========================");
 			}else if(response.statusCode == 503) {
-				console.log("------------------------");
-				console.log("Service Unavailable");
-				console.log("------------------------");
-				console.log("HTTP Error 503. The service is unavailable.");
-				console.log("------------------------");
+				console.log(`
+------------------------
+Service Unavailable
+HTTP Error 503. The service is unavailable.
+------------------------`);
 			}else if(response.statusCode == 400) {
 				console.log("Bad request.");
 			}else if(response.statusCode == 502) {
@@ -180,16 +206,21 @@ function doWhatRandomFileSays() {
 	// Reading random.txt 
 	fs.readFile("random.txt", "utf-8", function(error, data) {
 		var dataArray = data.split(',');
-		console.log("Data array: " + dataArray);
+		// Removing quotes at the end of string as specified in instruction format
 		var nameOfSong = dataArray[1].substring(1,dataArray[1].length-1);
-		console.log("Song name: " + nameOfSong);
 		// Sending the command and parameter to map function
 		performFunctionIfCommandIs(dataArray[0], nameOfSong);
 	});
 }
 
 function logData(command, parameter) {
-	fs.appendFile("log.txt", "\n"+ "----------------------------" + "\n" + moment().format("llll") + "\n" + command + " " + parameter + "\n" + "----------------------------", function(error) {
+	fs.appendFile("log.txt", `
+----------------------------
+${moment().format("llll")}
+${command}
+${parameter}
+----------------------------
+`, function(error) {
 		if(error) {
 			console.log(error);
 		}
